060a0983d83dd0636c243cc6a7a07b01fac70922
Nathan SAKKRIOU <nathan.sakkriou@soprasteria.com>
2025-05-07 11:55:50 +0200

    init

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,49 @@
+let firstSlideContainer = document.getElementsByClassName("slide--content")[0];
 
-let firstSlideContainer = 
-document.getElementsByClassName("slide--content")[0];
+let secondSlideContainer = document.getElementsByClassName(
+  "slide--content--one"
+)[0];
 
-let secondSlideContainer = 
-document.getElementsByClassName("slide--content--one")[0];
+let secondCanvas = document.getElementsByClassName("second--canvas")[0];
 
-let secondCanvas = 
-document.getElementsByClassName("second--canvas")[0];
+secondSlideContainer.setAttribute("style", "display:none");
+secondCanvas.setAttribute("style", "display:none");
 
-secondSlideContainer.setAttribute("style","display:none");
-secondCanvas.setAttribute("style","display:none");
+let thirdCanvas = document.getElementsByClassName("third--canvas")[0];
 
-let thirdCanvas = 
-document.getElementsByClassName("third--canvas")[0];
+thirdCanvas.setAttribute("style", "display:none");
 
-thirdCanvas.setAttribute("style","display:none");
+let containerToggleOne = setTimeout(function () {
+  firstSlideContainer.setAttribute("style", "display:none");
+  secondSlideContainer.setAttribute("style", "display:block");
+}, 3500);
 
+let removeFirstSlide = setTimeout(function () {
+  document
+    .getElementsByClassName("first--slide")[0]
+    .setAttribute("style", "display:none;");
+  secondCanvas.setAttribute("style", "display:block");
+}, 6500 + 500);
 
+let removeSecondCanvas = setTimeout(function () {
+  secondCanvas.setAttribute("style", "display:none");
+  thirdCanvas.setAttribute("style", "display:block");
+}, 9800 + 500);
 
-let containerToggleOne = setTimeout(function(){
-	firstSlideContainer.setAttribute("style","display:none");
-	secondSlideContainer.setAttribute("style","display:block");
-},2500);
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modalImg");
+const openBtns = document.querySelectorAll(".openModal");
 
+openBtns.forEach((openBtn) => {
+  openBtn.addEventListener("click", function () {
+    modal.style.display = "block";
+    modal.classList.add("fade-in");
+  });
+});
 
-let removeFirstSlide = setTimeout(function(){
-	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
-	secondCanvas.setAttribute("style","display:block");
-},6500);
-
-let removeSecondCanvas = setTimeout(function(){
-	secondCanvas.setAttribute("style","display:none");
-	thirdCanvas.setAttribute("style","display:block")
-},9800)
+window.onclick = function (e) {
+  if (e.target === modal) {
+    modal.style.display = "none";
+    modal.classList.remove("fade-in");
+  }
+};
